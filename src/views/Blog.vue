fda1e0174a1e369d3c5e6b9a4f3fdc24fa2f3473
Jose Villanueva <lv428694@gmail.com>
2022-05-18 00:39:44 -0500

    Update Blog.vue
    consumo de API

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,18 +1,37 @@
 <template>
-  <Titulo texto = 'Este es el título de mi blog'/>
+  <Titulo texto="Este es el título de mi blog" />
+  <div v-for="post in arrayBlog" :key="post.id">
+      <router-link :to="`/blog/${post.id}`">{{post.title}}</router-link>
+  </div>
 </template>
 
 <script>
-
-import Titulo from '../components/Titulo.vue'
+import Titulo from "../components/Titulo.vue";
 export default {
-    components: {
-        Titulo
-    }
-
-}
+  components: {
+    Titulo,
+  },
+  data() {
+    return {
+      arrayBlog: [],
+    };
+  },
+  methods: {
+    async consumirApi() {
+      try {
+        const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const array = await data.json();
+        this.arrayBlog = array;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.consumirApi();
+  },
+};
 </script>
 
 <style>
-
 </style>
