d101dd46ae6ecc2a05b61026280a8ec45540ca81
Jose Villanueva <lv428694@gmail.com>
2022-05-18 00:06:08 -0500

    add new route blog

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Este es la página de acerca de nosotros</h1>
   </div>
 </template>
---
+++ src/views/Blog.vue
@@ -0,0 +1,13 @@
+<template>
+  Esta es la pagina del blog
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
